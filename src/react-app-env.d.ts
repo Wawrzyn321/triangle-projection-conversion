fa22c706ad265819e92ec201a8cf364286c0071b
Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>
2026-04-28 09:03:47 +0200

    bump

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="react-scripts" />
 
+declare module '*.css';
+
 // dla quick refresh na devie - dajemy module.hot jako zależność w useEffect
 interface NodeModule {
     hot: any;
